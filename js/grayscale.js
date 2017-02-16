f298082579bd046804732a58306917b4084880f1
perugolate <paul.johnston@fu-berlin.de>
2017-02-16 22:14:08 +0100

    myLatLng

--- js/grayscale.js
+++ js/grayscale.js
@@ -182,7 +182,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(52.4594, 13.306);
+    var myLatLng = new google.maps.LatLng(52.4585, 13.305);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
